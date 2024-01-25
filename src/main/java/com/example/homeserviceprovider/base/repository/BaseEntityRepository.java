1e5e42c39e7de7461babdc32b62493929f7b296b
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:50:23 +0330

    create BaseEntityRepository

---
+++ src/main/java/com/example/homeserviceprovider/base/repository/BaseEntityRepository.java
@@ -0,0 +1,33 @@
+package com.example.homeserviceprovider.base.repository;
+
+import com.example.homeserviceprovider.base.domain.BaseEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BaseEntityRepository<T extends BaseEntity<ID>,ID extends Serializable> extends JpaRepository<T,ID> {
+      @Override
+      List<T> findAll();
+
+
+      @Override
+      <S extends T> S save(S entity);
+
+      @Override
+      Optional<T> findById(ID id);
+
+      @Override
+      boolean existsById(ID id);
+
+      @Override
+      void deleteById(ID id);
+
+      @Override
+      void delete(T entity);
+}
