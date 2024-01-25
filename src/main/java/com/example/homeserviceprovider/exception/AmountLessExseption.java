cc6f415f1385152fd863d1375bcc1b8f6d6c26e7
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:51:34 +0330

    create AmountLessExseption

---
+++ src/main/java/com/example/homeserviceprovider/exception/AmountLessExseption.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class AmountLessExseption extends RuntimeException {
+
+    public AmountLessExseption(String message) {
+        super(message);
+    }
+}
