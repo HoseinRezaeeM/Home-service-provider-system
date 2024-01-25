ab2d2a3936fdcaf8785f891a469f415914bc0832
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:45:57 +0330

    create BaseEntity

---
+++ src/main/java/com/example/homeserviceprovider/base/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.homeserviceprovider.base.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BaseEntity<ID extends Serializable> {
+      @Id @GeneratedValue(strategy = GenerationType.AUTO)
+      ID id;
+
+
+}
