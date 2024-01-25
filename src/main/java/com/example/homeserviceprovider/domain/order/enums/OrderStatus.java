642075ab6046fc96e4f77c7d2de47a768b47b1d9
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:47:58 +0330

    create OrderStatus

---
+++ src/main/java/com/example/homeserviceprovider/domain/order/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.example.homeserviceprovider.domain.order.enums;
+
+public enum OrderStatus {
+
+      WAITING_FOR_SPECIALIST_SUGGESTION,
+      WAITING_FOR_SPECIALIST_SELECTION,
+      WAITING_FOR_SPECIALIST_TO_COME,
+      STARTED,
+      DONE,
+      PAID
+}
