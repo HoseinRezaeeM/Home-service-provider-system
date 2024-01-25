a7147728e0bb90a51d17b7676f5259c5f77069ac
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:45:11 +0330

    create MainServices

---
+++ src/main/java/com/example/homeserviceprovider/domain/service/MainServices.java
@@ -0,0 +1,33 @@
+package com.example.homeserviceprovider.domain.service;
+
+import com.example.homeserviceprovider.base.domain.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MainServices extends BaseEntity<Long> {
+
+      @Column(nullable = false)
+      String name;
+      @OneToMany(mappedBy = "mainServices",cascade = CascadeType.ALL)
+      List<SubServices> subServicesList ;
+
+      public MainServices(String name) {
+            this.name = name;
+      }
+
+
+}
