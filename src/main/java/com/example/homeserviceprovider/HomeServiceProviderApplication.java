13148b57cdf14b5a9ef012777ad11bbbdcb23515
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:48:36 +0330

    HomeServiceProviderApplication

---
+++ src/main/java/com/example/homeserviceprovider/HomeServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceProviderApplication.class, args);
+	}
+
+}
