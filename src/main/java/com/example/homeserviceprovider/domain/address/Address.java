8319dcc462193571b74a373aaee6d760cdc92a50
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:46:17 +0330

    create Address

---
+++ src/main/java/com/example/homeserviceprovider/domain/address/Address.java
@@ -0,0 +1,34 @@
+package com.example.homeserviceprovider.domain.address;
+
+
+import com.example.homeserviceprovider.base.domain.BaseEntity;
+import com.example.homeserviceprovider.domain.user.Customer;
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+
+@Setter
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address extends BaseEntity<Long> {
+      @NonNull
+      String province;
+      @NonNull
+      String city;
+      @NonNull
+      String avenue;
+      @NonNull
+      String postalCode;
+      String houseNumber;
+      String moreDescription;
+      @ManyToOne(fetch = FetchType.EAGER)
+      Customer customer;
+}
