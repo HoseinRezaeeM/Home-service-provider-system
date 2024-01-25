b83a110c7e576559387059d4722f5ab7ea175107
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:51:22 +0330

    create AlphabetException

---
+++ src/main/java/com/example/homeserviceprovider/exception/AlphabetException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class AlphabetException extends RuntimeException {
+    public AlphabetException(String message) {
+        super(message);
+    }
+}
