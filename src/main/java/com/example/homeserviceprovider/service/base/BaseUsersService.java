8257de9f9fc52ae90b36a6ccfe26de21e54b6380
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:50:59 +0330

    create BaseUsersService

---
+++ src/main/java/com/example/homeserviceprovider/service/base/BaseUsersService.java
@@ -0,0 +1,10 @@
+package com.example.homeserviceprovider.service.base;
+
+import com.example.homeserviceprovider.base.service.BaseEntityService;
+import com.example.homeserviceprovider.domain.user.Users;
+
+import java.util.Optional;
+
+public interface BaseUsersService<T extends Users> extends BaseEntityService<T,Long> {
+      Optional<T> findByUsername(String email);
+}
