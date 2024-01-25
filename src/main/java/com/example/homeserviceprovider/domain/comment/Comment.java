68ec351770a791937f8a3d8fc35943d17e72834f
HoseinRezaei <rezaeen77@gmail.com>
2024-01-25 14:45:43 +0330

    create Comment

---
+++ src/main/java/com/example/homeserviceprovider/domain/comment/Comment.java
@@ -0,0 +1,24 @@
+package com.example.homeserviceprovider.domain.comment;
+
+
+import com.example.homeserviceprovider.base.domain.BaseEntity;
+import com.example.homeserviceprovider.domain.order.Order;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Comment extends BaseEntity<Long> {
+      int score;
+      String textComment;
+      @OneToOne
+      Order order;
+
+}
