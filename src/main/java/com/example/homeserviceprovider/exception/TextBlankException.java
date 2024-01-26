78238ca616829b71f545a09a686e6ef2c9393d16
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:40:13 +0330

    create TextBlankException

---
+++ src/main/java/com/example/homeserviceprovider/exception/TextBlankException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class TextBlankException extends RuntimeException {
+    public TextBlankException(String message) {
+        super(message);
+    }
+}
