d4f534b3210bc78a268e1aeb8f1896418d2a7c92
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:40:55 +0330

    create PasswordFormatException

---
+++ src/main/java/com/example/homeserviceprovider/exception/PasswordFormatException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class PasswordFormatException extends RuntimeException {
+
+    public PasswordFormatException(String message) {
+        super(message);
+    }
+}
