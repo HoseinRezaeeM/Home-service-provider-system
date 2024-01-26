4d10e95f99cfc3d67664bee26ff51914b1f5287e
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:42:45 +0330

    create EmailFormatException

---
+++ src/main/java/com/example/homeserviceprovider/exception/EmailFormatException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class EmailFormatException extends RuntimeException {
+
+    public EmailFormatException(String message) {
+        super(message);
+    }
+}
