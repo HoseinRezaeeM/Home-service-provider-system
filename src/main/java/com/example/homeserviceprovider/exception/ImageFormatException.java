60ea8571942741efebdaf0fad0132b2ce9c813f9
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:42:09 +0330

    create ImageFormatException

---
+++ src/main/java/com/example/homeserviceprovider/exception/ImageFormatException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class ImageFormatException extends RuntimeException {
+
+    public ImageFormatException(String message) {
+        super(message);
+    }
+}
