12642b796a9795f40e2b5a4abe125a36b049f976
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:39:21 +0330

    create SubServicesIsExistException

---
+++ src/main/java/com/example/homeserviceprovider/exception/SubServicesIsExistException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class SubServicesIsExistException extends RuntimeException {
+
+    public SubServicesIsExistException(String message) {
+        super(message);
+    }
+}
