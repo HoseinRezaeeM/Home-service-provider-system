c840acb437911899b1f406d99d98cae8f5f174ef
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:43:41 +0330

    create CustomerNotExistException

---
+++ src/main/java/com/example/homeserviceprovider/exception/CustomerNotExistException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class CustomerNotExistException extends RuntimeException {
+
+    public CustomerNotExistException(String message) {super(message);
+    }
+}
