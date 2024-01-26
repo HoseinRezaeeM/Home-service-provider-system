8a9fbfc88d9fd4d87b74a8173b9c5628f2235ea6
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:42:21 +0330

    create ImageSizeException

---
+++ src/main/java/com/example/homeserviceprovider/exception/ImageSizeException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class ImageSizeException extends RuntimeException {
+
+    public ImageSizeException(String message) {
+        super(message);
+    }
+}
