5d0954f04f91357c5f008dcaa4e1b3da3a6f9adf
HoseinRezaei <rezaeen77@gmail.com>
2024-01-26 14:38:38 +0330

    create SpecialistIsHoldsExistException

---
+++ src/main/java/com/example/homeserviceprovider/exception/SpecialistIsHoldsExistException.java
@@ -0,0 +1,8 @@
+package com.example.homeserviceprovider.exception;
+
+public class SpecialistIsHoldsExistException extends RuntimeException {
+
+    public SpecialistIsHoldsExistException(String message) {
+        super(message);
+    }
+}
