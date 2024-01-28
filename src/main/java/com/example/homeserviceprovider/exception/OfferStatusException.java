2529d8db6a586771a745d8ee7727ecb26e183724
HoseinRezaei <rezaeen77@gmail.com>
2024-01-28 21:29:59 +0330

    create OferStatusException

---
+++ src/main/java/com/example/homeserviceprovider/exception/OfferStatusException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class OfferStatusException extends RuntimeException {
+    public OfferStatusException(String message) {
+        super(message);
+    }
+}
