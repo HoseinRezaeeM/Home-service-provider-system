c6f47ea840a370b6f0496534d51446a2ab6339a0
HoseinRezaei <rezaeen77@gmail.com>
2024-01-29 11:16:18 +0330

    create AddressFormatException

---
+++ src/main/java/com/example/homeserviceprovider/exception/AddressFormatException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class AddressFormatException extends RuntimeException {
+    public AddressFormatException(String message) {
+        super(message);
+    }
+}
