6c0fafe8f73ad2bf6147ed16d23f1cc80d72847e
HoseinRezaei <rezaeen77@gmail.com>
2024-02-12 13:30:09 +0330

    edit Specialist

--- src/main/java/com/example/homeserviceprovider/domain/user/Specialist.java
+++ src/main/java/com/example/homeserviceprovider/domain/user/Specialist.java
@@ -17,7 +17,6 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Specialist extends Users {
       @Lob
@@ -26,25 +25,23 @@ public class Specialist extends Users {
       Long credit;
       @Enumerated(value = EnumType.STRING)
       SpecialistStatus status;
-      @ManyToMany(mappedBy = "specialistList",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-      List<SubServices> subServicesList ;
+      @ManyToMany(mappedBy = "specialistList", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+      List<SubServices> subServicesList;
       @OneToMany(mappedBy = "specialist")
-      List<Offer> offerList ;
+      List<Offer> offerList;
       String province;
 
-      public Specialist(String firstname, String lastname, String email, String password,String province,
-                     byte[] image) {
+      public Specialist(String firstname, String lastname, String email, String password, String province,
+                        byte[] image) {
             super(firstname, lastname, email, password, Role.SPECIALIST);
             this.score = 0;
             this.status = SpecialistStatus.NEW;
             this.image = image;
-            this.credit =0L;
-            this.province=province;
+            this.credit = 0L;
+            this.province = province;
       }
 
-      public Specialist(long id) {
-            super(id);
-      }
+
 
       public void addSubServices(SubServices subServices) {
             this.subServicesList.add(subServices);
@@ -54,5 +51,18 @@ public class Specialist extends Users {
       public void deleteSubServices(SubServices subServices) {
             this.subServicesList.remove(subServices);
             subServices.getSpecialistList().remove(this);
+      }
+
+      public void delay(int hours) {
+            this.score = this.score - hours;
+            checkRate();
+      }
+
+      private void checkRate() {
+            if (this.score < 0) {
+                  this.setIsActive(false);
+                  this.status = SpecialistStatus.AWAITING;
+                  this.score = 0;
+            }
       }
 }
