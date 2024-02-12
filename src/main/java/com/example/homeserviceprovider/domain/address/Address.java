cf3b55bdf93a5adc31a73e0dc867afb0f9327c3d
HoseinRezaei <rezaeen77@gmail.com>
2024-02-12 13:16:41 +0330

    edit add Constructor

--- src/main/java/com/example/homeserviceprovider/domain/address/Address.java
+++ src/main/java/com/example/homeserviceprovider/domain/address/Address.java
@@ -31,4 +31,14 @@ public class Address extends BaseEntity<Long> {
       String moreDescription;
       @ManyToOne(fetch = FetchType.EAGER)
       Customer customer;
+
+      public Address(@NonNull String province, @NonNull String city, @NonNull String avenue,
+                     @NonNull String postalCode, String houseNumber, String moreDescription) {
+            this.province = province;
+            this.city = city;
+            this.avenue = avenue;
+            this.postalCode = postalCode;
+            this.houseNumber = houseNumber;
+            this.moreDescription = moreDescription;
+      }
 }
