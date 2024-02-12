278143c589e4baac51d2d700fc0f081f9752ffbe
HoseinRezaei <rezaeen77@gmail.com>
2024-02-12 13:14:28 +0330

    create VerifyCodeException

---
+++ src/main/java/com/example/homeserviceprovider/exception/VerifyCodeException.java
@@ -0,0 +1,7 @@
+package com.example.homeserviceprovider.exception;
+
+public class VerifyCodeException extends RuntimeException {
+    public VerifyCodeException(String message) {
+        super(message);
+    }
+}
