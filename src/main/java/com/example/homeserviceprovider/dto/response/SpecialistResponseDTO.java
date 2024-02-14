ec33132207eb84802b57497b8a8f0bc2d9e2d028
HoseinRezaei <rezaeen77@gmail.com>
2024-02-14 16:05:31 +0330

    create SpecialistResponseDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/response/SpecialistResponseDTO.java
@@ -0,0 +1,31 @@
+package com.example.homeserviceprovider.dto.response;
+
+import com.example.homeserviceprovider.domain.user.enums.SpecialistStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class SpecialistResponseDTO {
+
+    Long specialistId;
+    String firstname;
+    String lastname;
+    String email;
+    Boolean isActive;
+    SpecialistStatus specialistStatus;
+    double score;
+    Long credit;
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+    LocalDateTime registrationTime;
+
+}
