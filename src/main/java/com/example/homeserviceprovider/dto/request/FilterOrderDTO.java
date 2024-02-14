c334f78e2cafe47302218e0ef8951803b2b804c3
HoseinRezaei <rezaeen77@gmail.com>
2024-02-14 16:08:29 +0330

    create FilterOrderDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/FilterOrderDTO.java
@@ -0,0 +1,32 @@
+package com.example.homeserviceprovider.dto.request;
+
+import com.example.homeserviceprovider.domain.order.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FilterOrderDTO {
+
+    Long mainServiceId;
+    Long jobId;
+    String description;
+    OrderStatus orderStatus;
+
+    Long minProposedPrice;
+    Long maxProposedPrice;
+
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+    LocalDateTime minOrderRegistrationDate;
+    @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+    LocalDateTime maxOrderRegistrationDate;
+
+}
