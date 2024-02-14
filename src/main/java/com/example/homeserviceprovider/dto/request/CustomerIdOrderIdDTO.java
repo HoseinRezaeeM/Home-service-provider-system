049d0927977eb5d4e1cd048e16084f527b24b25c
HoseinRezaei <rezaeen77@gmail.com>
2024-02-14 16:09:09 +0330

    create CustomerIdOrderIdDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/CustomerIdOrderIdDTO.java
@@ -0,0 +1,17 @@
+package com.example.homeserviceprovider.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults( level = AccessLevel.PRIVATE)
+public class CustomerIdOrderIdDTO {
+
+    Long customerId;
+    Long orderId;
+}
