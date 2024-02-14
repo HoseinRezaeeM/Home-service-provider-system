2bc2e16e15af97e6f04d978805dc48fc5d227517
HoseinRezaei <rezaeen77@gmail.com>
2024-02-14 16:08:18 +0330

    create FilterUserDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/FilterUserDTO.java
@@ -0,0 +1,42 @@
+package com.example.homeserviceprovider.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FilterUserDTO {
+
+     String userType;
+     String firstname;
+     String lastname;
+     String username;
+     Boolean isActive;
+     String userStatus;
+
+     @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+     LocalDateTime minUserCreationAt;
+     @JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
+     LocalDateTime maxUserCreationAt;
+
+     Integer minNumberOfOperation;
+     Integer maxNumberOfOperation;
+
+     Integer minNumberOfDoneOperation;
+     Integer maxNumberOfDoneOperation;
+
+     Double minScore;
+     Double maxScore;
+
+     Long minCredit;
+     Long maxCredit;
+
+}
