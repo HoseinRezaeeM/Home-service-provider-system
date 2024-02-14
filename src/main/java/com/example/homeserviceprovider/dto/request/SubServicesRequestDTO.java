c1c6dce725a85362ac44680121a030f3eac599ea
HoseinRezaei <rezaeen77@gmail.com>
2024-02-14 16:04:38 +0330

    edit SubServicesRequestDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/SubServicesRequestDTO.java
@@ -0,0 +1,20 @@
+package com.example.homeserviceprovider.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SubServicesRequestDTO {
+
+    String mainServiceRequest;
+    String name;
+    String description;
+    Long basePrice;
+
+}
