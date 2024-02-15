6e30f7eae9e0024021aa0edf2c42e75365e42a20
HoseinRezaei <rezaeen77@gmail.com>
2024-02-15 11:05:34 +0330

    create BalancePageDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/BalancePageDTO.java
@@ -0,0 +1,20 @@
+package com.example.homeserviceprovider.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BalancePageDTO {
+    Long price;
+    String captcha;
+    String hidden;
+    String image;
+    Long customertId;
+
+}
