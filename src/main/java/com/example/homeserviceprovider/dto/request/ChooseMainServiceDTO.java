a76bca4fe651c8c92fadcb14ba0959602a8b3754
HoseinRezaei <rezaeen77@gmail.com>
2024-02-15 11:05:04 +0330

    create ChooseMainServiceDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/ChooseMainServiceDTO.java
@@ -0,0 +1,19 @@
+package com.example.homeserviceprovider.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class ChooseMainServiceDTO {
+
+    Long mainServiceId;
+    String mainServiceName;
+
+}
