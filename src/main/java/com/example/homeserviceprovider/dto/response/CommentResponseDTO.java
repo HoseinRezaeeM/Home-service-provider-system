eda7877cbe1d5085b0266cb840076422cb12cc1c
HoseinRezaei <rezaeen77@gmail.com>
2024-02-15 11:03:26 +0330

    create CommentResponseDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/response/CommentResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.homeserviceprovider.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class CommentResponseDTO {
+
+     String comment;
+     int score;
+
+}
