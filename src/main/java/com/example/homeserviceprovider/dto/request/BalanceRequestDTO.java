9fa6b888e759b0d99e87ac06d43c800e0eeb7ee4
HoseinRezaei <rezaeen77@gmail.com>
2024-02-15 11:05:25 +0330

    create BalanceRequestDTO

---
+++ src/main/java/com/example/homeserviceprovider/dto/request/BalanceRequestDTO.java
@@ -0,0 +1,37 @@
+package com.example.homeserviceprovider.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BalanceRequestDTO {
+
+    @NotNull
+    String captcha;
+    @NotNull
+    String hidden;
+    @NotNull
+    @Pattern(regexp = "[0-9]{13}(?:[0-9]{3})?$",message = "card number must be 16 digit")
+    String number;
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z]*$",message = "please check your name . you can't enter space or digit")
+    String name;
+    @Pattern(regexp = "\\b\\d{3}\\b",message = "your cvc must be 3 digit")
+    String cvc;
+    @NotNull
+    String year;
+    @NotNull
+    String month;
+    @Valid
+    CustomerIdPriceDTO customerIdPriceDTO;
+
+}
