4f2ca8f352d7425bf7667c0969cbf16d08f44e8f
HoseinRezaei <rezaeen77@gmail.com>
2024-02-16 23:00:41 +0330

    create SecurityConfig

---
+++ src/main/java/com/example/homeserviceprovider/security/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.example.homeserviceprovider.security.config;
+
+
+import com.example.homeserviceprovider.service.Impl.CustomerUserDetailsService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static com.example.homeserviceprovider.domain.user.enums.Role.*;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests((requests) -> requests
+                                .requestMatchers("/registration/**").permitAll()
+                                .requestMatchers("/admin/**").hasAnyAuthority(ADMIN.name())
+                                .requestMatchers("/customer/**").hasAuthority(CUSTOMER.name())
+                                .requestMatchers("/specialist/**").hasAuthority(SPECIALIST.name())
+                                .anyRequest().authenticated()
+
+//                .logout()
+//                .clearAuthentication(true)
+//                .invalidateHttpSession(true)
+//                .permitAll();
+
+                )
+                .authenticationProvider(new CustomAuthProvider(
+                        (CustomerUserDetailsService) userDetailsService(), passwordEncoder()))
+                .httpBasic(basic -> {})
+        ;
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomerUserDetailsService();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+}
